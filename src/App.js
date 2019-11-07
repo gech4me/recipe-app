78b766a3afff0d982909795013fafe52b1b6360e
Getachew Mulat <gech4me@gmail.com>
2019-11-07 13:03:54 +0300

    Recipes retrived successfully

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '53d6181b';
+  const APP_KEY = 'bec2effb00a6d049a171d5cc2faca269';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+      getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+      const data = await response.json();
+      setRecipes(data.hits);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <form action="#" className="search-form">
+          <input className="search-bar" type="text" />
+          <button className="search-button" type="submit">Search</button>
+        </form>
+      </div>
   );
-}
+};
 
 export default App;
